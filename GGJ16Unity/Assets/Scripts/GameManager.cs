17ed4b7deae9a28336af7298b689ef02405b3e41
REUBEN BRENNER-ADAMS (RIT Student) <rmb1952@ad.rit.edu>
2016-01-30 23:03:26 -0500

    Added multiplier and score text to UI

--- GGJ16Unity/Assets/Scripts/GameManager.cs
+++ GGJ16Unity/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject[] multiplierImages;
     public GameObject[] thingsToMakeInvisible;
 
     GameObject[] beatLines;
@@ -10,9 +11,10 @@ public class GameManager : MonoBehaviour
     GameObject[] notes;
     GameObject[] players;
 
-    public static int score=0;
+    public static int score = 0;
     public static int noteCount = 0;
     public static int multiplier;
+    public GUIText scoreText;
 
 	// Use this for initialization
 	void Start()
@@ -35,15 +37,64 @@ public class GameManager : MonoBehaviour
         if(noteCount >= 5 && noteCount <10)
         {
             multiplier = 2;
+            for(int i = 0; i < 4; i++)
+            {
+                if(i == 1)
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+                else
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
         }
-        if(noteCount >= 10 && noteCount < 15)
+        else if(noteCount >= 10 && noteCount < 15)
         {
             multiplier = 3;
+            for(int i = 0; i < 4; i++)
+            {
+                if(i == 2)
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+                else
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
         }
-        if(noteCount >= 15 && noteCount < 20)
+        else if(noteCount >= 15 && noteCount < 20)
         {
             multiplier = 4;
+            for(int i = 0; i < 4; i++)
+            {
+                if(i == 3)
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+                else
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
         }
+        else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+                else
+                {
+                    multiplierImages[i].GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
+        }
+
+        scoreText.GetComponent<GUIText>().text = score.ToString();
     }
 
     void StartGame()
